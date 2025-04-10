9c94e0227981b32bf67b21959165ea5af32cfafa
zeke <40004347+KAJdev@users.noreply.github.com>
2025-04-09 17:30:24 -0800

    fix

--- .runpod/src/handler.py
+++ .runpod/src/handler.py
@@ -17,17 +17,16 @@ async def handler(job):
     inputs = job["input"]
     run_id = inputs["run_id"]
     args = inputs["args"]
-    
+
     # set_config_env_vars(args)
-    
+
     # Set output directory
     output_dir = os.path.join(BASE_VOLUME, get_output_dir(run_id))
     args["output_dir"] = output_dir
-    
+
     # First save args to a temporary config file
     config_path = "/workspace/test_config.yaml"
-    
-        
+
     # Add run_name and job_id to args before saving
     args["run_name"] = run_id
     args["runpod_job_id"] = runpod_job_id
@@ -35,27 +34,26 @@ async def handler(job):
     yaml_data = yaml.dump(args, default_flow_style=False)
     with open(config_path, "w") as file:
         file.write(yaml_data)
-    
+
     # Handle credentials
-    credentials = inputs["credentials"]
-    os.environ["WANDB_API_KEY"] = credentials["wandb_api_key"]
-    os.environ["HF_TOKEN"] = credentials["hf_token"]
-    
+    credentials = inputs.get("credentials", {})
+
+    if "wandb_api_key" in credentials:
+        os.environ["WANDB_API_KEY"] = credentials["wandb_api_key"]
+    if "hf_token" in credentials:
+        os.environ["HF_TOKEN"] = credentials["hf_token"]
+
     validate_env(logger, runpod_job_id)
     login(token=os.environ["HF_TOKEN"])
-    
+
     logger.info(f"Starting Training.")
     async for result in train(config_path):  # Pass the config path instead of args
         logger.info(result)
     logger.info(f"Training Complete.")
-    
+
     # Cleanup
     del os.environ["WANDB_API_KEY"]
     del os.environ["HF_TOKEN"]
 
 
-
-runpod.serverless.start({
-    "handler": handler,
-    "return_aggregate_stream": True
-})
+runpod.serverless.start({"handler": handler, "return_aggregate_stream": True})
